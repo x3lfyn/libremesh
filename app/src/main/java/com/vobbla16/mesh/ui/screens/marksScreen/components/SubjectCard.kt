4442e19a0f1be99313a3cf5263e496f17d8dd236
vobbla16 <novikovprojects@gmail.com>
2023-12-19 19:32:56 +0300

    fix SubjectCard

--- app/src/main/java/com/vobbla16/mesh/ui/screens/marksScreen/components/SubjectCard.kt
+++ app/src/main/java/com/vobbla16/mesh/ui/screens/marksScreen/components/SubjectCard.kt
@@ -37,43 +37,52 @@ import kotlinx.datetime.LocalDate
 @OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun SubjectCard(
-    subject: MarksSubjectModel, opened: Boolean, onClick: () -> Unit, modifier: Modifier = Modifier
+    subject: MarksSubjectModel,
+    opened: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     OutlinedCard(modifier = modifier.fillMaxWidth(), onClick = onClick) {
-        FlowRow(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(6.dp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = subject.subjectName, style = MaterialTheme.typography.titleMedium
-            )
+            FlowRow(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(6.dp)
+            ) {
+                Text(
+                    text = subject.subjectName, style = MaterialTheme.typography.titleMedium
+                )
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                subject.periods.filter {
-                    it.finalMark != null
-                }.forEach { subj ->
-                    CheckMark(
-                        mark = subj.finalMark!!,
-                        bgColor = MaterialTheme.colorScheme.secondaryContainer,
-                        type = CheckMarkType.SingleCheck,
-                        modifier = Modifier.padding(4.dp)
-                    )
-                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    subject.periods.filter {
+                        it.finalMark != null
+                    }.forEach { subj ->
+                        CheckMark(
+                            mark = subj.finalMark!!,
+                            bgColor = MaterialTheme.colorScheme.secondaryContainer,
+                            type = CheckMarkType.SingleCheck,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
 
-                subject.yearMark?.let { yearMark ->
-                    Icon(
-                        imageVector = Icons.Default.ArrowForward,
-                        contentDescription = "arrow forward icon"
-                    )
-                    CheckMark(
-                        mark = yearMark,
-                        bgColor = MaterialTheme.colorScheme.tertiaryContainer,
-                        type = CheckMarkType.DoubleCheck,
-                        modifier = Modifier.padding(4.dp)
-                    )
+                    subject.yearMark?.let { yearMark ->
+                        Icon(
+                            imageVector = Icons.Default.ArrowForward,
+                            contentDescription = "arrow forward icon"
+                        )
+                        CheckMark(
+                            mark = yearMark,
+                            bgColor = MaterialTheme.colorScheme.tertiaryContainer,
+                            type = CheckMarkType.DoubleCheck,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
                 }
             }
 
@@ -87,8 +96,15 @@ fun SubjectCard(
                     contentDescription = "arrow icon",
                     modifier = Modifier.rotate(rotationDegree)
                 )
+            } else {
+                Text(
+                    text = "Нет оценок",
+                    style = MaterialTheme.typography.labelMedium,
+                    modifier = Modifier.padding(4.dp, 0.dp)
+                )
             }
         }
+
 
         AnimatedVisibility(visible = opened) {
             Column {
@@ -387,4 +403,81 @@ fun SubjectCardPreview3() {
         periods = emptyList()
     )
     SubjectCard(subject = subj, opened = true, onClick = {}, modifier = Modifier.padding(8.dp))
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+fun SubjectCardPreview4() {
+    val subj = MarksSubjectModel(
+        subjectName = "Основы духовно-нравственной культуры народов России 1111111111111111111111111111111111111",
+        average = MarkValue(fiveScale = 4.51f, hundredScale = 90.0f),
+        yearMark = null,
+        examMark = null,
+        attestationMark = null,
+        periods = listOf(
+            Period(
+                name = "I триместр",
+                average = MarkValue(fiveScale = 4.53f, hundredScale = 90.53f),
+                finalMark = null,
+                startDate = LocalDate(2022, 9, 1),
+                endDate = LocalDate(2022, 11, 30),
+                isYearMark = false,
+                marks = listOf(
+                    Mark(
+                        comment = null,
+                        controlFormName = "Практическая работа",
+                        date = LocalDate(2022, 9, 1),
+                        gradeType = GradeType.Five,
+                        isPoint = false,
+                        pointDate = null,
+                        topic = "Механика.Неравномерное движение",
+                        value = MarkValue(fiveScale = 4.0f, hundredScale = 80.0f),
+                        weight = 2
+                    ), Mark(
+                        comment = null,
+                        controlFormName = "Комбинированная работа",
+                        date = LocalDate(2022, 9, 1),
+                        gradeType = GradeType.Five,
+                        isPoint = false,
+                        pointDate = null,
+                        topic = "Механика.Неравномерное движение",
+                        value = MarkValue(fiveScale = 5.0f, hundredScale = 100.0f),
+                        weight = 2
+                    ), Mark(
+                        comment = null,
+                        controlFormName = "Решение задач",
+                        date = LocalDate(2022, 9, 1),
+                        gradeType = GradeType.Five,
+                        isPoint = false,
+                        pointDate = null,
+                        topic = "Механика.Неравномерное движение",
+                        value = MarkValue(fiveScale = 4.0f, hundredScale = 80.0f),
+                        weight = 2
+                    ), Mark(
+                        comment = null,
+                        controlFormName = "Решение задач",
+                        date = LocalDate(2022, 9, 1),
+                        gradeType = GradeType.Five,
+                        isPoint = false,
+                        pointDate = null,
+                        topic = "Механика.Неравномерное движение",
+                        value = MarkValue(fiveScale = 4.0f, hundredScale = 80.0f),
+                        weight = 2
+                    ), Mark(
+                        comment = "Там какой то коммент к оценке",
+                        controlFormName = "Решение задач",
+                        date = LocalDate(2022, 9, 1),
+                        gradeType = GradeType.Five,
+                        isPoint = false,
+                        pointDate = null,
+                        topic = "Механика.Неравномерное движение",
+                        value = MarkValue(fiveScale = 5.0f, hundredScale = 100.0f),
+                        weight = 2
+                    )
+                )
+            )
+        )
+    )
+    SubjectCard(subject = subj, opened = false, onClick = {}, modifier = Modifier.padding(8.dp))
 }
